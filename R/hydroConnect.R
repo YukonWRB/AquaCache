83c030e7f71accbb91269853f14f01155ac30e88
gdelaplante <91148465+gdelaplante@users.noreply.github.com>
2023-08-11 16:42:19 +0000

    First steps to working with psql

---
+++ R/hydroConnect.R
@@ -0,0 +1,37 @@
+#' Connect to hydrology database
+#'
+#' @description
+#' `r lifecycle::badge("stable")`
+#'
+#' This function exists to facilitate connecting to the hydrology database, especially if the database type and connection method changes in the future. Cannot be used to create a new database, use [DBI::dbConnect()] with the appropriate driver (e.g. [RSQLite::SQLite()]) for that purpose.
+#'
+#' @param name Database name.
+#' @param host Database host address.
+#' @param port Connection port.
+#' @param username Username. Refrain from using username with write privileges unless you absolutely know what you're doing.
+#' @param password Password.
+#' @param silent TRUE suppresses messages except for errors.
+#'
+#' @return A connection to the database.
+#'
+#' @seealso [DB_browse_ts()], [DB_browse_spatial()]to browse the database contents; [DB_get_meta()] to extract metadata; [DB_get_ts()] to extract timeseries information; and [DB_get_spatial()] to extract spatial content.
+#' @export
+#'
+
+hydroConnect <- function(name = "hydromet", host = "localhost", port = "5432", username = "postgres", password = "SnowFa11ing", silent = FALSE){
+
+  tryCatch({
+    hydro <- DBI::dbConnect(drv = RPostgres::Postgres(),
+                            dbname = name,
+                            host = host,
+                            port = port,
+                            user = username,
+                            password = password)
+    if (!silent){
+      print("Remember to disconnect using DBI::dbDisconnect() when finished.")
+    }
+    return(hydro)
+  }, error = function(e){
+    stop("Connection failed.")
+  })
+}
