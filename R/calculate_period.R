5c14c823adb05f8dfba1b5e31a615c286f109139
gdelaplante <91148465+gdelaplante@users.noreply.github.com>
2023-12-01 23:16:11 +0000

    Hived off the period calculation portion of some functions to its own function.

---
+++ R/calculate_period.R
@@ -0,0 +1,103 @@
+#' Calculate periodicity of data and add a column
+#'
+#' Calculates a period for temporal data and prepares a column names 'period' with ISO8601 formated periods for import to postgres database. Will identify changes to periodicity within data, for example moving from 1-hour intervals to 6-hour intervals.
+#'
+#' @param data The data.frame for which to calculate periodicity. Must contain, at minimum, columns named 'datetime', 'value', 'grade', 'approval', but only 'datetime' needs to have no NAs.
+#' @param timeseries_id The ID of the timeseries for which to calculate periodicity. Used to fetch any data points lacking a period, as well as to search for additional data points if there are too few to calculate a period in the provided `data`.
+#' @param con A connection to the database, created with [DBI::dbConnect()] or using the utility function [hydrometConnect()].
+#'
+#' @return A list of two objects: a data.frame with calculated periods and a boolean delete flag, true if rows were fetched from the database (and thus need to be deleted prior to appending) or false if no data needs to be replaced
+#' @export
+
+calculate_period <- function(data, timeseries_id, con = hydrometConnect())
+{
+  # Get datetimes from the earliest missing period to calculate necessary values, as some might be missing
+  no_period <- DBI::dbGetQuery(con, paste0("SELECT datetime, value, grade, approval FROM measurements_continuous WHERE timeseries_id = ", timeseries_id, " AND datetime >= (SELECT MIN(datetime) FROM measurements_continuous WHERE period IS NULL AND timeseries_id = ", timeseries_id, ");"))
+  if (nrow(no_period) > 0){
+    ts <- rbind(ts, no_period)
+  }
+  ts <- ts[order(ts$datetime) ,] #Sort ascending
+  diffs <- as.numeric(diff(ts$datetime), units = "hours")
+  smoothed_diffs <- zoo::rollmedian(diffs, k = 3, fill = NA, align = "center")
+  # Initialize variables to track changes
+  consecutive_count <- 0
+  changes <- data.frame()
+  last_diff <- 0
+  if (length(smoothed_diffs) > 0) {
+    for (j in 1:length(smoothed_diffs)) {
+      if (!is.na(smoothed_diffs[j]) && smoothed_diffs[j] < 25 && smoothed_diffs[j] != last_diff) { # Check if smoothed interval is less than threshold, which is set to more than a whole day (greatest interval possible is 24 hours) as well as not the same as the last recorded diff
+        consecutive_count <- consecutive_count + 1
+        if (consecutive_count == 3) { # At three consecutive new measurements it's starting to look like a pattern
+          last_diff <- smoothed_diffs[j]
+          change <- data.frame(datetime = ts$datetime[j-3],
+                               period = last_diff)
+          changes <- rbind(changes, change)
+          consecutive_count <- 0
+        }
+      } else {
+        consecutive_count <- 0
+      }
+    }
+  }
+
+  # Calculate the duration in days, hours, minutes, and seconds and assign to the right location in ts
+  ts$period <- NA
+  if (nrow(changes) > 0){
+    for (j in 1:nrow(changes)){
+      days <- floor(changes$period[j] / 24)
+      remaining_hours <- changes$period[j] %% 24
+      minutes <- floor((remaining_hours - floor(remaining_hours)) * 60)
+      seconds <- round(((remaining_hours - floor(remaining_hours)) * 60 - minutes) * 60)
+      ts[ts$datetime == changes$datetime[j], "period"] <- paste("P", days, "DT", floor(remaining_hours), "H", minutes, "M", seconds, "S", sep = "")
+    }
+    #carry non-na's forward and backwards, if applicable
+    ts$period <- zoo::na.locf(zoo::na.locf(ts$period, na.rm = FALSE), fromLast=TRUE)
+
+  } else { #In this case there were too few measurements to conclusively determine a period so pull a few from the DB and redo the calculation
+    no_period <- DBI::dbGetQuery(con, paste0("SELECT datetime, value, grade, approval FROM measurements_continuous WHERE timeseries_id = ", timeseries_id, " ORDER BY datetime DESC LIMIT 10;"))
+    no_period$period <- NA
+    ts <- rbind(ts, no_period)
+    ts <- ts[order(ts$datetime), ]
+    diffs <- as.numeric(diff(ts$datetime), units = "hours")
+    smoothed_diffs <- zoo::rollmedian(diffs, k = 3, fill = NA, align = "center")
+    consecutive_count <- 0
+    changes <- data.frame()
+    last_diff <- 0
+    if (length(smoothed_diffs) > 0){
+      for (j in 1:length(smoothed_diffs)) {
+        if (!is.na(smoothed_diffs[j]) && smoothed_diffs[j] < 25 && smoothed_diffs[j] != last_diff) {
+          consecutive_count <- consecutive_count + 1
+          if (consecutive_count == 3) {
+            last_diff <- smoothed_diffs[j]
+            change <- data.frame(datetime = ts$datetime[j-3],
+                                 period = last_diff)
+            changes <- rbind(changes, change)
+            consecutive_count <- 0
+          }
+        } else {
+          consecutive_count <- 0
+        }
+      }
+    }
+    if (nrow(changes) > 0){
+      for (k in 1:nrow(changes)){
+        days <- floor(changes$period[k] / 24)
+        remaining_hours <- changes$period[k] %% 24
+        minutes <- floor((remaining_hours - floor(remaining_hours)) * 60)
+        seconds <- round(((remaining_hours - floor(remaining_hours)) * 60 - minutes) * 60)
+        ts[ts$datetime == changes$datetime[k], "period"] <- paste("P", days, "DT", floor(remaining_hours), "H", minutes, "M", seconds, "S", sep = "")
+      }
+      #carry non-na's forward and backwards, if applicable
+      ts$period <- zoo::na.locf(zoo::na.locf(ts$period, na.rm = FALSE), fromLast=TRUE)
+    } else {
+      ts$period <- NULL
+    }
+  }
+  if (nrow(no_period) > 0){
+    delete_flag <- TRUE
+  } else {
+    delete_flag <- FALSE
+  }
+  result <- list(ts = ts, delete_flag = delete_flag)
+  return(result)
+} # End of function
--- R/getNewContinuous.R
+++ R/getNewContinuous.R
@@ -72,90 +72,17 @@
       }
 
       ts <- do.call(source_fx, args_list) #Get the data using the args_list
-
       ts <- ts[!is.na(ts$value) , ]
 
       if (nrow(ts) > 0){
+        #assign a period to the data
         if (period_type == "instantaneous"){ #Period is always 0 for instantaneous data
           ts$period <- "00:00:00"
-          no_period <- data.frame() # Created here for use later
+          delete_flag <- FALSE
         } else if ((period_type != "instantaneous") & !("period" %in% names(ts))) { #period_types of mean, median, min, max should all have a period
-          # Get datetimes from the earliest missing period to calculate necessary values, as some might be missing
-          no_period <- DBI::dbGetQuery(con, paste0("SELECT datetime, value, grade, approval FROM measurements_continuous WHERE timeseries_id = ", tsid, " AND datetime >= (SELECT MIN(datetime) FROM measurements_continuous WHERE period IS NULL AND timeseries_id = ", tsid, ");"))
-          if (nrow(no_period) > 0){
-            ts <- rbind(ts, no_period)
-          }
-          ts <- ts[order(ts$datetime) ,] #Sort ascending
-          diffs <- as.numeric(diff(ts$datetime), units = "hours")
-          smoothed_diffs <- zoo::rollmedian(diffs, k = 3, fill = NA, align = "center")
-          # Initialize variables to track changes
-          consecutive_count <- 0
-          changes <- data.frame()
-          last_diff <- 0
-          for (j in 1:length(smoothed_diffs)) {
-            if (!is.na(smoothed_diffs[j]) && smoothed_diffs[j] < 25 && smoothed_diffs[j] != last_diff) { # Check if smoothed interval is less than threshold, which is set to more than a whole day (greatest interval possible is 24 hours) as well as not the same as the last recorded diff
-              consecutive_count <- consecutive_count + 1
-              if (consecutive_count == 3) { # At three consecutive new measurements it's starting to look like a pattern
-                last_diff <- smoothed_diffs[j]
-                change <- data.frame(datetime = ts$datetime[j-3],
-                                     period = last_diff)
-                changes <- rbind(changes, change)
-                consecutive_count <- 0
-              }
-            } else {
-              consecutive_count <- 0
-            }
-          }
-          # Calculate the duration in days, hours, minutes, and seconds and assign to the right location in ts
-          ts$period <- NA
-          if (nrow(changes) > 0){
-            for (j in 1:nrow(changes)){
-              days <- floor(changes$period[j] / 24)
-              remaining_hours <- changes$period[j] %% 24
-              minutes <- floor((remaining_hours - floor(remaining_hours)) * 60)
-              seconds <- round(((remaining_hours - floor(remaining_hours)) * 60 - minutes) * 60)
-              ts[ts$datetime == changes$datetime[j]+10*60, "period"] <- paste("P", days, "DT", floor(remaining_hours), "H", minutes, "M", seconds, "S", sep = "")
-            }
-            #carry non-na's forward and backwards, if applicable
-            ts$period <- zoo::na.locf(zoo::na.locf(ts$period, na.rm = FALSE), fromLast=TRUE)
-
-          } else { #In this case there were too few measurements to conclusively determine a period so pull a few from the DB and redo the calculation
-            no_period <- DBI::dbGetQuery(con, paste0("SELECT datetime, value, grade, approval FROM measurements_continuous WHERE timeseries_id = ", tsid, " ORDER BY datetime DESC LIMIT 10;"))
-            ts <- rbind(ts, no_period)
-            ts <- ts[order(ts$datetime), ]
-            diffs <- as.numeric(diff(ts$datetime), units = "hours")
-            smoothed_diffs <- zoo::rollmedian(diffs, k = 3, fill = NA, align = "center")
-            consecutive_count <- 0
-            changes <- data.frame()
-            last_diff <- 0
-            for (j in 1:length(smoothed_diffs)) {
-              if (!is.na(smoothed_diffs[j]) && smoothed_diffs[j] < 25 && smoothed_diffs[j] != last_diff) {
-                consecutive_count <- consecutive_count + 1
-                if (consecutive_count == 3) {
-                  last_diff <- smoothed_diffs[j]
-                  change <- data.frame(datetime = ts$datetime[j-3],
-                                       period = last_diff)
-                  changes <- rbind(changes, change)
-                  consecutive_count <- 0
-                }
-              } else {
-                consecutive_count <- 0
-              }
-            }
-            if (nrow(changes) > 0){
-              for (k in 1:nrow(changes)){
-                days <- floor(changes$period[k] / 24)
-                remaining_hours <- changes$period[k] %% 24
-                minutes <- floor((remaining_hours - floor(remaining_hours)) * 60)
-                seconds <- round(((remaining_hours - floor(remaining_hours)) * 60 - minutes) * 60)
-                ts[ts$datetime == changes$datetime[k]+10*60, "period"] <- paste("P", days, "DT", floor(remaining_hours), "H", minutes, "M", seconds, "S", sep = "")
-              }
-              #carry non-na's forward and backwards, if applicable
-              ts$period <- zoo::na.locf(zoo::na.locf(ts$period, na.rm = FALSE), fromLast=TRUE)
-            } else {
-              ts$period <- NULL
-            }
-          }
+          period_res <- calculate_period(data = ts, timeseries_id = tsid)
+          ts <- period_res$ts
+          delete_flag <- period_res$delete_flag
         } else { #Check to make sure that the supplied period can actually be coerced to a period
           check <- lubridate::period(unique(ts$period))
           if (NA %in% check){
@@ -163,11 +90,10 @@
           }
         }
         ts$timeseries_id <- tsid
-        ts$imputed <- FALSE
         # The column for "imputed" defaults to FALSE in the DB, so even though it is NOT NULL it doesn't need to be specified UNLESS this function gets modified to impute values.
         DBI::dbWithTransaction(
           con, {
-            if (nrow(no_period) > 0){
+            if (delete_flag){
               DBI::dbExecute(con, paste0("DELETE FROM measurements_continous WHERE datetime >= '", min(ts$datetime), "' AND timeseries_id = ", tsid, ";"))
             }
             DBI::dbAppendTable(con, "measurements_continuous", ts)
