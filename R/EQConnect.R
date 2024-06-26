aab5165cc4acf3f57112bf78af76c6aeda9722d3
gdelaplante <91148465+gdelaplante@users.noreply.github.com>
2024-06-26 17:52:31 +0000

    Problem solved by dependent package update

--- R/EQConnect.R
+++ R/EQConnect.R
@@ -13,18 +13,21 @@
 #' @export
 #'
 
-EQConnect <- function(path = "//carver/infosys/EQWin/WR/DB/Water_Resources.mdb;", silent = FALSE){
+EQConnect <- function(path = "//carver/infosys/EQWin/WR/DB/Water Resources.mdb", silent = FALSE){
 
-  warning("This function is causing R to crash as of 2024-06-25. Unsure what the issue is. Contact the Data Scientist if you require programmatic access to the EQWin database in the meantime.")
-  # tryCatch({
-  #   EQWin <- DBI::dbConnect(drv = odbc::odbc(), .connection_string = paste0("Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=", path))
-  #   DBI::dbCanConnect(odbc::odbc(), .connection_string = paste0("Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=", path))
-  #   if (!silent) {
-  #     print("Remember to disconnect using DBI::dbDisconnect() when finished.")
-  #   }
-  #   return(EQWin)
-  # }, error = function(e) {
-  #   stop("EQWin connection failed. Do you need to install the Access database engine? Check the help file.")
-  # })
-
+  # Check if the file exists
+  if (!file.exists(path)) {
+    stop("The specified database file does not exist.")
+  }
+  
+  tryCatch({
+    EQWin <- DBI::dbConnect(drv = odbc::odbc(), .connection_string = paste0("Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=", path))
+    if (!silent) {
+      message("Remember to disconnect using DBI::dbDisconnect() when finished.")
+    }
+    return(EQWin)
+  }, error = function(e) {
+    stop("EQWin connection failed: ", e$message, ". Do you need to install the Access database engine? Check the help file.")
+  })
+  
 }
