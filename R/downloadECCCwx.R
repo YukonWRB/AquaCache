4d46677373b20be1fb404f88dadb29ae1cc8a0e3
gdelaplante <91148465+gdelaplante@users.noreply.github.com>
2024-10-11 23:18:01 +0000

    incorrect table reference

--- R/downloadECCCwx.R
+++ R/downloadECCCwx.R
@@ -48,22 +48,22 @@ downloadECCCwx <- function(location, parameter_id, start_datetime, end_datetime 
   }, error = function(e) {
     stop("Failed to convert parameter end_datetime to POSIXct.")
   })
-
+  
   # Check if there already exists a temporary file with the required interval, location, start_datetime, and end_datetime.
   files <- list.files(paste0(tempdir(), "/downloadECCCwx"))
-
+  
   if (length(files) == 0) {
     file_exists <- FALSE
   } else {
     name <- paste0(location, "_", interval, "_", substr(start_datetime, 1, 10), "_", substr(end_datetime, 1, 10), ".rdata")
     if (name %in% files) {
-     load(paste0(tempdir(), "/downloadECCCwx/", location, "_", interval, "_", substr(start_datetime, 1, 10), "_", substr(end_datetime, 1, 10), ".rdata"))
+      load(paste0(tempdir(), "/downloadECCCwx/", location, "_", interval, "_", substr(start_datetime, 1, 10), "_", substr(end_datetime, 1, 10), ".rdata"))
       file_exists <- TRUE
     } else {
       file_exists <- FALSE
     }
   }
-
+  
   # If there is no file that matches necessary use, download
   if (!file_exists) {
     dl <- suppressMessages(weathercan::weather_dl(location, start = start_datetime, interval = interval, time_disp = "UTC", quiet = TRUE))
@@ -71,32 +71,28 @@ downloadECCCwx <- function(location, parameter_id, start_datetime, end_datetime 
     dir.create(paste0(tempdir(), "/downloadECCCwx"), showWarnings = FALSE)
     save(dl, file = paste0(tempdir(), "/downloadECCCwx/", location, "_", interval, "_", substr(start_datetime, 1, 10), "_", substr(end_datetime, 1, 10), ".rdata"))
   }
-
+  
   #Extract the necessary information according to the parameter_id
   if (nrow(dl) > 0) {
     if ("time" %in% names(dl)) { # then it must be hourly
       data <- data.frame(datetime = dl$time,
                          value = dl[[parameter_id]]) #Note the different subsetting because dl is a tibble.
-      data <- data[data$datetime > start_datetime & data$datetime < end_datetime & !is.na(data$value) , ]
-      if (nrow(data) > 0) {
-        data$grade <- 9 #Undefined
-        data$approval <- 5 #Undefined
-      }
     } else if (("date" %in% names(dl)) & !("time" %in% names(dl))) { #Must be daily or more
       data <- data.frame(datetime = as.POSIXct(dl$date, tz = "UTC") + 30*60*60,  #Observations building daily values end at 6 UTC on following day (so values reported on the 24th include hours 07 to 23 on the 24th plus 00 to 06 on the 25th)
                          value = dl[[parameter_id]]) #Note the different subsetting because dl is a tibble.
-      data <- data[data$datetime > start_datetime & data$datetime < end_datetime & !is.na(data$value) , ]
-      if (nrow(data) > 0) {
-        data$grade <- 9 #Undefined
-        data$approval <- 5 #Undefined
-      }
     } else {
       stop("downloadECCCwx: Column named 'time' or 'date' has not been found.")
+    }
+    data <- data[data$datetime > start_datetime & data$datetime < end_datetime & !is.na(data$value) , ]
+    if (nrow(data) > 0) {
+      data$grade <- 9 #Unspecified
+      data$approval <- 5 #Unspecified
+      data$qualifier <- 7 #Unspecified
     }
   } else {
     data <- data.frame()
   }
-
+  
   return(data)
 }
 
