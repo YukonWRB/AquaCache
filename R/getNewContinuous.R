8bcf834f1ef74f953c63e54dceac50e09407ba71
gdelaplante <91148465+gdelaplante@users.noreply.github.com>
2024-11-05 23:17:24 +0000

    .

--- R/getNewContinuous.R
+++ R/getNewContinuous.R
@@ -69,7 +69,7 @@ getNewContinuous <- function(con = NULL, timeseries_id = "all", active = 'defaul
   qualifier_unknown <- DBI::dbGetQuery(con, "SELECT qualifier_type_id FROM qualifier_types WHERE qualifier_type_code = 'UNK';")[1,1]
   if (is.na(qualifier_unknown)) {
     stop("getNewContinuous: Could not find qualifier type 'Unknown' in the database.")
-  # }
+  }
   
   message("Fetching new continuous data with getNewContinuous...")
   if (interactive()) {
