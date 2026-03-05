250440c4eacee66636259704a0c6f3663adcb407
gdelaplante <91148465+gdelaplante@users.noreply.github.com>
2026-03-05 12:07:44 -0700

    discrete guideline storage improvements

--- inst/patches/DEV_patch_35.R
+++ inst/patches/DEV_patch_35.R
@@ -106,6 +106,104 @@ tryCatch(
       GROUP BY ts.timeseries_id, loc.location_id, mtypes.media_type_fr, params.param_name_fr, params.unit_default, ag.aggregation_type_fr, lz.z_meters, dc.conversion_m;'
     )
 
+    # Add some columns to 'discrete.guidelines' and further build out ways of distinguishing guidelines
+
+    # Start by wiping the guidelines table, there should be nothing in there anyways at this point and it will make it easier to work with the new structure. If there are guidelines in there, they should be re-added after the patch is applied.
+    DBI::dbExecute(con, "DELETE FROM discrete.guidelines;")
+
+    # Create a new table for 'publisher' information for guidelines; link the current column 'publisher' to this new table
+    DBI::dbExecute(
+      con,
+      "CREATE TABLE IF NOT EXISTS discrete.guideline_publishers (
+        publisher_id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+        publisher_name TEXT UNIQUE,
+        publisher_name_fr TEXT UNIQUE,
+        country TEXT,
+        prov_terr_state TEXT
+      );"
+    )
+    # Alter the current publishers column to reference the new table
+    # Check if exists first, if not, add it and then link it to the new table.
+    exist <- DBI::dbGetQuery(
+      con,
+      "SELECT column_name FROM information_schema.columns WHERE table_schema = 'discrete' AND table_name = 'guidelines' AND column_name = 'publisher';"
+    )
+    if (nrow(exist) == 0) {
+      DBI::dbExecute(
+        con,
+        "ALTER TABLE discrete.guidelines
+       ALTER COLUMN publisher TYPE INTEGER USING (publisher::INTEGER),
+       ADD CONSTRAINT fk_publisher FOREIGN KEY (publisher) REFERENCES discrete.guideline_publishers(publisher_id) ON UPDATE CASCADE ON DELETE CASCADE;"
+      )
+    }
+
+    # Create a new table to hold 'series' information for guidelines; link the current column 'series' to this new table. Series are associated with a publisher.
+    DBI::dbExecute(
+      con,
+      "CREATE TABLE IF NOT EXISTS discrete.guideline_series (
+        series_id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY ,
+        series_name TEXT UNIQUE NOT NULL,
+        series_name_fr TEXT UNIQUE,
+        publisher_id INTEGER NOT NULL REFERENCES discrete.guideline_publishers(publisher_id) ON UPDATE CASCADE ON DELETE CASCADE
+      );"
+    )
+
+    check <- DBI::dbGetQuery(
+      con,
+      "SELECT column_name FROM information_schema.columns WHERE table_schema = 'discrete' AND table_name = 'guidelines' AND column_name = 'series';"
+    )
+    if (nrow(check) == 0) {
+      DBI::dbExecute(
+        con,
+        "ALTER TABLE discrete.guidelines ADD COLUMN IF NOT EXISTS series INTEGER REFERENCES discrete.guideline_series(series_id) ON UPDATE CASCADE ON DELETE CASCADE;"
+      )
+    }
+
+    # Guidelines can apply to multiple media types, so we need a linking table for that
+    DBI::dbExecute(
+      con,
+      "CREATE TABLE IF NOT EXISTS discrete.guidelines_media_types (
+        guideline_id INTEGER REFERENCES discrete.guidelines(guideline_id) ON UPDATE CASCADE ON DELETE CASCADE,
+        media_id INTEGER REFERENCES public.media_types(media_id) ON UPDATE CASCADE ON DELETE CASCADE,
+        PRIMARY KEY (guideline_id, media_id)
+      );"
+    )
+    DBI::dbExecute(
+      con,
+      "COMMENT ON TABLE discrete.guidelines_media_types IS 'Linking table to associate guidelines with media types. A guideline can apply to multiple media types, and a media type can have multiple guidelines.';"
+    )
+
+    # Guidelines can also apply to multiple fractions (i.e. dissolved, total, etc.), so we need a linking table for that as well
+    DBI::dbExecute(
+      con,
+      "CREATE TABLE IF NOT EXISTS discrete.guidelines_fractions (
+        guideline_id INTEGER REFERENCES discrete.guidelines(guideline_id) ON UPDATE CASCADE ON DELETE CASCADE,
+        fraction_id INTEGER REFERENCES discrete.sample_fractions(sample_fraction_id) ON UPDATE CASCADE ON DELETE CASCADE,
+        PRIMARY KEY (guideline_id, fraction_id)
+      );"
+    )
+    DBI::dbExecute(
+      con,
+      "COMMENT ON TABLE discrete.guidelines_fractions IS 'Linking table to associate guidelines with sample fractions. A guideline can apply to multiple fractions, and a fraction can have multiple guidelines.';"
+    )
+
+    # Drop the guidlines.sample_fraction_id column as this is now being handled in the new linking table
+    check <- DBI::dbGetQuery(
+      con,
+      "SELECT column_name FROM information_schema.columns WHERE table_schema = 'discrete' AND table_name = 'guidelines' AND column_name = 'sample_fraction_id';"
+    )
+    if (nrow(check) > 0) {
+      DBI::dbExecute(
+        con,
+        "ALTER TABLE discrete.guidelines DROP COLUMN IF EXISTS sample_fraction_id;"
+      )
+    }
+
+    DBI::dbExecute(
+      con,
+      "DROP TRIGGER IF EXISTS trg_enforce_sample_fraction ON discrete.guidelines;"
+    )
+
     # Wrap things up ##################
     # Update the version_info table
     DBI::dbExecute(
